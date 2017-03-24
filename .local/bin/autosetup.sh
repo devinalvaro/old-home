f0d2860356abdaaca3f4156c72e9ad4853066466
Devin Alvaro <devin.alvaro@gmail.com>
2017-03-24 22:09:52 +0700

    Add many more setups to autosetup.sh

--- .local/bin/autosetup.sh
+++ .local/bin/autosetup.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
 
+# git
+sudo dnf install git
+
+# home
+cd ~
+git init
+git remote add origin https://github.com/devinalvaro/home
+git fetch
+git checkout -t origin/master
+
+# pip
+sudo dnf install python-pip
+sudo dnf install python3-pip
+sudo pip install --upgrade pip
+sudo pip2 install --upgrade pip
+
+# tldr
+sudo pip install tldr
+
+# neovim
+sudo dnf install neovim
+sudo pip install neovim
+sudo pip2 install neovim
+
+# zsh
+sudo dnf install zsh
+chsh -s $(which zsh)
+
 # oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-# arc-theme
-git clone https://github.com/horst3180/arc-theme --depth 1 && cd arc-theme
-./autogen.sh --prefix=/usr
-sudo make install
-rm -rf arc-theme
+# tmux
+sudo dnf install tmux
+# put:
+#   tmux new-session -A -s "0"
+# in terminal custom command
 
-# la-capitaine
-git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git ~/.icons
-
-# tldr
-sudo npm install -g tldr
+# xclip
+sudo dnf install xclip
 
 # base16-shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+source ~/.config/base16-shell/scripts/base16-tomorrow-night.sh
+
+# arc-theme
+sudo dnf install autoconf
+sudo dnf install automake
+sudo dnf install pkgconfig # pkg-config in Debian
+sudo dnf install gtk3-devel # libgtk-3-dev in Debian
+
+git clone https://github.com/horst3180/arc-theme --depth 1
+cd arc-theme
+./autogen.sh --prefix=/usr
+sudo make install
+cd ..
+rm -rf arc-theme
+
+# la-capitaine
+git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git ~/.icons/la-capitaine
+
+# capitaine-cursors
+git clone https://github.com/keeferrourke/capitaine-cursors.git
+cp -pr capitaine-cursors/bin/xcursor ~/.icons/capitaine-cursors
+rm -rf capitaine-cursors
