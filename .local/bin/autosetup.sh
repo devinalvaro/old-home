49af16ba5d9a5704841d998d43050405695dfd0b
Devin Alvaro <devin.alvaro@gmail.com>
2017-03-26 18:30:25 +0700

    Separate autosetup and autotheme

--- .local/bin/autosetup.sh
+++ .local/bin/autosetup.sh
@@ -47,27 +47,3 @@ sudo dnf install tig
 
 # xclip
 sudo dnf install xclip
-
-# arc-theme
-sudo dnf install autoconf
-sudo dnf install automake
-sudo dnf install pkgconfig # pkg-config in Debian
-sudo dnf install gtk3-devel # libgtk-3-dev in Debian
-
-git clone https://github.com/horst3180/arc-theme --depth 1
-cd arc-theme
-./autogen.sh --prefix=/usr
-sudo make install
-cd ..
-rm -rf arc-theme
-
-# la-capitaine
-git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git ~/.icons/la-capitaine
-
-# capitaine-cursors
-git clone https://github.com/keeferrourke/capitaine-cursors.git
-cp -pr capitaine-cursors/bin/xcursor ~/.icons/capitaine-cursors
-rm -rf capitaine-cursors
-
-# gogh
-wget -O xt https://git.io/vKOB6 && chmod +x xt && ./xt
