ee1333da32291659ff7dccaeabccb8036e977e0a
Devin Alvaro <devin.alvaro@gmail.com>
2018-02-18 13:53:57 +0700

    Add $FZF_IGNORE_DIRS and $FZF_IGNORE_FILES

--- .zsh/variables.zsh
+++ .zsh/variables.zsh
@@ -13,9 +13,12 @@ export YARN=$HOME/.yarn
 export PATH=$COMPOSER_HOME/vendor/bin:$GOPATH/bin:$LOCAL/bin:$YARN/bin:$PATH
 
 # fzf
+FZF_IGNORE_DIRS="__pycache__,node_modules,Sdk"
+FZF_IGNORE_FILES="$FZF_IGNORE_DIRS,tags"
+
 export FZF_DEFAULT_OPTS=--inline-info
-export FZF_DEFAULT_COMMAND='rg --follow --files --no-ignore --maxdepth 8 --glob "!{__pycache__/*,node_modules/*,Sdk/*,tags}" 2> /dev/null'
-export FZF_ALT_C_COMMAND='fd --follow --type d --no-ignore --max-depth 8 --exclude "{__pycache__,node_modules,Sdk}" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --follow --files --no-ignore --maxdepth 8 --glob "!{$FZF_IGNORE_FILES}" 2> /dev/null'
+export FZF_ALT_C_COMMAND='fd --follow --type d --no-ignore --max-depth 8 --exclude "{$FZF_IGNORE_DIRS}" 2> /dev/null'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 
 # lc
@@ -23,38 +26,38 @@ export LC_COLLATE='C'
 
 # spaceship
 export SPACESHIP_PROMPT_ORDER=(
-  time # Time stampts section
-  # user # Username section
-  dir # Current directory section
-  host # Hostname section
-  git # Git section (git_branch + git_status)
-  # hg # Mercurial section (hg_branch  + hg_status)
-  package # Package version
-  node # Node.js section
-  # ruby # Ruby section
-  # elixir # Elixir section
-  # xcode # Xcode section
-  # swift # Swift section
-  golang # Go section
-  php # PHP section
-  # rust # Rust section
-  haskell # Haskell Stack section
-  # julia # Julia section
-  docker # Docker section
-  # aws # Amazon Web Services section
-  venv # virtualenv section
-  conda # conda virtualenv section
-  pyenv # Pyenv section
-  # dotnet # .NET section
-  # ember # Ember.js section
-  # kubecontext # Kubectl context section
-  exec_time # Execution time
-  line_sep # Line break
-  # battery # Battery level and status
-  # vi_mode # Vi-mode indicator
-  jobs # Background jobs indicator
-  exit_code # Exit code section
-  char # Prompt character
+  time
+  # user
+  dir
+  host
+  git
+  # hg
+  package
+  node
+  # ruby
+  # elixir
+  # xcode
+  # swift
+  golang
+  php
+  # rust
+  haskell
+  # julia
+  docker
+  # aws
+  venv
+  conda
+  pyenv
+  # dotnet
+  # ember
+  # kubecontext
+  exec_time
+  line_sep
+  # battery
+  # vi_mode
+  jobs
+  exit_code
+  char
 )
 export SPACESHIP_PROMPT_PREFIXES_SHOW=false
 export SPACESHIP_DIR_TRUNC=0
