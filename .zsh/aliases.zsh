32afd38f041bb43d10cdfbf5c270ef367c04bc86
Devin Alvaro <devin.alvaro@gmail.com>
2018-03-07 22:23:22 +0700

    Rethink alias and unalias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,56 +1,15 @@
 # modifier aliases
-alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias http='http --style=bw'
-alias ls='ls --group-directories-first --color=tty'
+alias ls='ls --color=tty'
 
 # shortener aliases
+alias g='git'
 alias pac='trizen'
-alias paste='xclip -o -selection clipboard'
+alias py='python'
 alias xo='xdg-open'
-alias yank='xclip -i -selection clipboard -rmlastnl'
 
 # navigation aliases
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-alias ~='cd ~'
-
-## alias groups ##
-
-# git
-alias ga='git add'
-alias gaa='git add --all'
-alias gbr='git branch'
-alias gcl='git clone'
-alias gcn='git clean'
-alias gco='git checkout'
-alias gct='git commit'
-alias gca='git commit --amend'
-alias gcm='git commit -m'
-alias gdf='git diff'
-alias gdc='git diff --cached'
-alias gfe='git fetch'
-alias glg='git log'
-alias gmr='git merge'
-alias gmv='git mv'
-alias gp='git push'
-alias gpl='git pull'
-alias grm='git rm'
-alias grs='git reset'
-alias grv='git revert'
-alias gsh='git stash'
-alias gss='git status -s'
-alias gsa='git status -s -uall'
-
-# online-judge-tools
-alias ojc='rm -rf a.out test'
-alias ojd='oj download'
-alias ojt='oj test 2>&1 | less'
-alias ojs='oj submit'
-
-# python
-alias py='python'
-alias py2='python2'
-alias py3='python3'
+alias ...='../..'
+alias ....='../../..'
+alias .....='../../../..'
+alias ......='../../../../..'
--- .zsh/unaliases.zsh
+++ .zsh/unaliases.zsh
@@ -1,7 +1,18 @@
 # oh-my-zsh
+unalias -- -
+unalias 1
+unalias 2
+unalias 3
+unalias 4
+unalias 5
+unalias 6
+unalias 7
+unalias 8
+unalias 9
 unalias _
 unalias afind
 unalias d
+unalias grep
 unalias history
 unalias l
 unalias la
