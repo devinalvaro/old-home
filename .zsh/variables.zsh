f39cdeea0cd41db0db8d28b21cf1f54e0fdc6b02
Devin Alvaro <devin.alvaro@gmail.com>
2018-03-24 20:44:36 +0700

    Decrease fzf depth limits

--- .zsh/variables.zsh
+++ .zsh/variables.zsh
@@ -11,12 +11,12 @@ export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.yarn/bin:$PATH
 
 # fzf
-export FZF_IGNORE_DIRS="__pycache__,node_modules,Sdk"
+export FZF_IGNORE_DIRS="__pycache__,node_modules"
 export FZF_IGNORE_FILES="$FZF_IGNORE_DIRS,tags"
 
 export FZF_DEFAULT_OPTS=--inline-info
-export FZF_DEFAULT_COMMAND='rg --follow --files --no-ignore --maxdepth 8 --glob "!{$FZF_IGNORE_FILES}" 2> /dev/null'
-export FZF_ALT_C_COMMAND='fd --follow --type d --no-ignore --max-depth 8 --exclude "{$FZF_IGNORE_DIRS}" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --follow --files --no-ignore --maxdepth 4 --glob "!{$FZF_IGNORE_FILES}" 2> /dev/null'
+export FZF_ALT_C_COMMAND='fd --follow --type d --no-ignore --max-depth 4 --exclude "{$FZF_IGNORE_DIRS}" 2> /dev/null'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 
 # lc
