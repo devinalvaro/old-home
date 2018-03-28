dbaab6468cd9c64c4e07edef4ac84f97d335479c
Devin Alvaro <devin.alvaro@gmail.com>
2018-03-28 20:35:43 +0700

    Add activate() to zsh

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,2 +1,20 @@
 # gitignore.io
-function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
+function gi() { curl -L -s https://www.gitignore.io/api/$@; }
+
+# virtualenv
+function activate() {
+    dir=$1
+
+    venv_dirs=('.env' '.venv' 'env' 'venv' 'ENV' 'env.bak' 'venv.bak')
+    for venv_dir in ${venv_dirs}; do
+        if [ -z $dir ] && [ -d $venv_dir ]; then
+            dir=$venv_dir
+        fi
+    done
+
+    if [ ! -z $dir ]; then
+        source $dir/bin/activate;
+    else
+        echo "No virtualenv directory detected."
+    fi
+}
