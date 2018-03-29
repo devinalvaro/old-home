355b4389d2a47aff36549d8d23912f2671cad3b1
Devin Alvaro <devin.alvaro@gmail.com>
2018-03-29 12:11:29 +0700

    Have some scripts as functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,5 +1,45 @@
+# adb
+function adbrestart() {
+    adb kill-server
+    sudo adb start-server
+}
+
+# clipboard
+function paste() {
+    if type "xclip" > /dev/null 2>&1; then
+        xclip -out -selection clipboard
+    elif type"xsel" > /dev/null 2>&1; then
+        xsel --output --clipboard
+    fi
+}
+
+function yank() {
+    if type "xclip" > /dev/null 2>&1; then
+        xclip -in -selection clipboard
+    elif type"xsel" > /dev/null 2>&1; then
+        xsel --input --clipboard
+    fi
+}
+
 # gitignore.io
 function gi() { curl -L -s https://www.gitignore.io/api/$@; }
+
+# truecolor
+function truecolor() {
+    awk 'BEGIN{
+        s="/\\/\\/\\/\\/\\"; s=s s s s s s s s s s s s s s s s s s s s s s s;
+        for (colnum = 0; colnum<256; colnum++) {
+            r = 255-(colnum*255/255);
+            g = (colnum*510/255);
+            b = (colnum*255/255);
+            if (g>255) g = 510-g;
+            printf "\033[48;2;%d;%d;%dm", r,g,b;
+            printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+            printf "%s\033[0m", substr(s,colnum+1,1);
+        }
+        printf "\n";
+    }'
+}
 
 # virtualenv
 function activate() {
