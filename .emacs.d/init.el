0b7e753ee58e66a49db8a6788046cd52e7e4f228
Devin Alvaro <devin.alvaro@gmail.com>
2018-04-12 01:31:02 +0700

    Add and configure evil plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,11 @@
+;; Definitions
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Modes
+(blink-cursor-mode 0)
+(menu-bar-mode -1)
+
+;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
@@ -10,17 +18,49 @@
 (use-package atom-one-dark-theme
   :ensure t
   :config
-  (load-theme 'atom-one-dark t))
+  (load-theme 'atom-one-dark t)
+  (set-face-foreground 'font-lock-comment-face "#8c919a"))
 
 (use-package better-defaults
-  :ensure t
-  :config
-  (blink-cursor-mode 0))
+  :ensure t)
 
 (use-package evil
   :ensure t
+  :init
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-Y-yank-to-eol t)
   :config
-  (evil-mode t))
+  (evil-mode t)
+
+  (use-package evil-commentary
+    :ensure t
+    :config
+    (evil-commentary-mode))
+
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode))
+
+  (use-package evil-magit
+    :ensure t
+    :config
+    (evil-define-key evil-magit-state magit-mode-map "?" 'evil-search-backward))
+
+  (use-package evil-matchit
+    :ensure t
+    :config
+    (global-evil-matchit-mode))
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-visualstar
+    :ensure t
+    :config
+    (global-evil-visualstar-mode)))
 
 (use-package magit
   :ensure t)
