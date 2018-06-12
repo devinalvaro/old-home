e7e35ef6b48f7ae047addc33111fee2942021b49
Devin Alvaro <devin.alvaro@gmail.com>
2018-06-12 16:22:14 +0700

    Remove nvm from loadings.zsh

--- .zsh/loadings.zsh
+++ .zsh/loadings.zsh
@@ -4,10 +4,9 @@ function lazy_load() {
     # $3: name of the command to run after it's loaded
     # $4+: argv to be passed to $3
 
-    local -a names
-    names=("${(@s: :)${1}}")
-
+    local -a names=("${(@s: :)${1}}")
     unalias "${names[@]}"
+
     . $2
 
     shift 2
@@ -15,17 +14,12 @@ function lazy_load() {
 }
 
 function group_lazy_load() {
-    local script
-    script=$1
-
+    local script=$1
     shift 1
     for cmd in "$@"; do
         alias $cmd="lazy_load \"$*\" $script $cmd"
     done
 }
-
-# nvm
-group_lazy_load "/usr/share/nvm/init-nvm.sh" nvm
 
 # rvm
 group_lazy_load "/usr/share/rvm/scripts/rvm" rvm
