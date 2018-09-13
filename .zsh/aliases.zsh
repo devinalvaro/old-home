06fdae2eea87757b1ac67c05c5118dff917fd6de
Devin Alvaro <devin.alvaro@gmail.com>
2018-09-13 20:07:01 +0700

    Alias kubectl to k

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,17 +4,15 @@ alias less='less --tabs=4'
 alias ls='ls --color=auto --group-directories-first'
 
 # shortener aliases
+alias c='docker-compose'
+alias d='docker'
 alias g='git'
+alias k='kubectl'
 alias py='python'
 alias rb='ruby'
 alias vi="$EDITOR"
 alias vim="$EDITOR"
 alias xo='xdg-open'
-
-alias c='docker-compose'
-alias cx='docker-compose exec'
-alias d='docker'
-alias dx='docker exec'
 
 alias paste='xclip -out -selection clipboard'
 alias yank='xclip -in -selection clipboard'
