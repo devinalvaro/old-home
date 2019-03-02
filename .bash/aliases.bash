da4875de27d16ba45efaf3661a2f8d7a9fe896f6
Devin Alvaro <devin.alvaro@gmail.com>
2019-03-02 16:39:14 +0700

    Introduce 'p' and 'v' aliases

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -8,11 +8,10 @@ alias c='docker-compose'
 alias d='docker'
 alias g='git'
 alias k='kubectl'
-alias pe='pipenv'
+alias p='pipenv'
 alias py='python'
 alias rb='ruby'
-alias vi="$EDITOR"
-alias vim="$EDITOR"
+alias v="$EDITOR"
 alias xo='xdg-open'
 
 alias paste='xclip -out -selection clipboard'
