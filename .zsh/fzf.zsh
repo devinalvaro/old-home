df0ab322d29d1aa2fefcf140e417e2c421f7abc8
Devin Alvaro <devin.alvaro@gmail.com>
2019-04-12 23:13:31 +0700

    Detach fzf completion

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -4,10 +4,6 @@ if [ ! "$PATH" = "*$HOME/.fzf/bin*" ]; then
     export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[ "$-" = "*i*" ] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
-
 # Key bindings
 # ------------
 source "$HOME/.fzf/shell/key-bindings.zsh"
