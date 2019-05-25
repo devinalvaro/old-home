9702c59d28100dd486b95f231ee7884d372bce48
Devin Alvaro <devin.alvaro@gmail.com>
2019-05-25 11:26:37 +0700

    Alias e=exa

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,11 +4,11 @@ alias v="$EDITOR"
 # modifier aliases
 alias http='http --style=bw'
 alias less='less --tabs=4'
-alias ls='ls --color=auto --group-directories-first'
 
 # shortener aliases
 alias c='docker-compose'
 alias d='docker'
+alias e='exa'
 alias g='git'
 alias py='python'
 alias xo='xdg-open'
