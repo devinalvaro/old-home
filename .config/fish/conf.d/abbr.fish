f54a78752b836eb5efc82d30a661275e3d55b390
Devin Alvaro <devin.alvaro@gmail.com>
2019-05-30 22:23:15 +0700

    Improve abbr.fish

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,8 +1,9 @@
-abbr -a c docker-compose
+abbr -a c cargo
 abbr -a d docker
 abbr -a e exa
 abbr -a g git
+abbr -a p python
 abbr -a v "$EDITOR"
 
-abbr -a py python
+abbr -a dc docker-compose
 abbr -a xo xdg-open
