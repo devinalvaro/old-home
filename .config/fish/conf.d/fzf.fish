1770f380954c82502dcf2bd9817d7b23f794ec6d
Devin Alvaro <devin.alvaro@gmail.com>
2019-07-07 22:35:00 +0700

    Add to fzf options

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,4 +1,5 @@
 set -x FZF_DEFAULT_OPTS '--ansi --exact --inline-info'
+set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --delimiter : --nth -1.."
 set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview='cat {}' --preview-window=hidden --bind=alt-p:toggle-preview"
 
 if type -q fd;
