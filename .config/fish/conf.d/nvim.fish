d99e67dc698b7a7f20e43934a30701fb1f46f34b
Devin Alvaro <devin.alvaro@gmail.com>
2019-07-09 23:48:05 +0700

    Remove tmux

--- .config/fish/conf.d/nvim.fish
+++ .config/fish/conf.d/nvim.fish
@@ -2,7 +2,7 @@ type -q vi  ; and set -x EDITOR 'vi'
 type -q vim ; and set -x EDITOR 'vim'
 type -q nvim; and set -x EDITOR 'nvim'
 
-if set -q TMUX;
+if set -q KITTY_WINDOW_ID;
     if set -q NVIM_LISTEN_ADDRESS; and type -q nvr;
         set -x EDITOR 'nvr'
         set -x GIT_EDITOR "$EDITOR --remote-wait"
--- .config/fish/conf.d/tmux.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive;
-    if set -q KITTY_WINDOW_ID; and not set -q TMUX;
-        exec tmux new-session -A -s main
-    end
-end
