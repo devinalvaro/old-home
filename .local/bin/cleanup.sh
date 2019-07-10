19db46c50f047abd3ad5a3045389898eb76cc06f
Devin Alvaro <devin.alvaro@gmail.com>
2019-07-11 01:55:43 +0700

    Improve scripts

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+set -eu
+
+# fzf
 rm -f ~/.local/share/fzf/fzf_history/*
 
+# neovim
 rm -f ~/.local/share/nvim/shada/main.shada
 rm -f ~/.local/share/nvim/undo/*
 rm -f ~/.local/share/nvim/view/*
--- .local/bin/gitignore.sh
+++ .local/bin/gitignore.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+set -eu
+
+# stash current .gitignore
 if [ -f .gitignore ]; then
     mv .gitignore /tmp/.gitignore
 fi
 
+# append untracked files
 > .gitignore
 git status --porcelain | grep '^??' | cut -c4- | sed 's/^/\//' >> .gitignore
 
+# append non-hidden directories in current path
 find . -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*' | cut -c 3- | sed 's/^/\//' | sed 's/$/\//' >> .gitignore
+
+# sort and remove duplicates
 sort -u .gitignore -o .gitignore
 
+# unstash stashed .gitignore if there's no difference
 if cmp -s .gitignore /tmp/.gitignore; then
     mv /tmp/.gitignore .gitignore 2> /dev/null
+else
+    rm -f /tmp/.gitignore
 fi
