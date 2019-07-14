21962c934552d124b04ec70db802c5ede329c022
Devin Alvaro <devin.alvaro@gmail.com>
2019-07-14 21:04:42 +0700

    Remove an excess line in fzf.fish

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -6,4 +6,3 @@ if type -q fd;
     set -x FZF_DEFAULT_COMMAND 'fd --type=file --color=always --follow --hidden'
     set -x FZF_DIRECTORY_COMMAND 'fd --type=directory --color=always --follow --no-ignore-vcs'
 end
-
