73847bddb0acff04088525ad75c4a420a840d03c
Devin Alvaro <devin.alvaro@gmail.com>
2019-08-06 01:05:06 +0800

    Remove automatic nvim execution

--- .config/fish/conf.d/nvim.fish
+++ .config/fish/conf.d/nvim.fish
@@ -2,11 +2,7 @@ type -q vi  ; and set -x EDITOR 'vi'
 type -q vim ; and set -x EDITOR 'vim'
 type -q nvim; and set -x EDITOR 'nvim'
 
-if set -q KITTY_WINDOW_ID;
-    if set -q NVIM_LISTEN_ADDRESS; and type -q nvr;
-        set -x EDITOR 'nvr'
-        set -x GIT_EDITOR "$EDITOR --remote-wait"
-    else
-        exec $EDITOR
-    end
+if set -q NVIM_LISTEN_ADDRESS; and type -q nvr;
+    set -x EDITOR 'nvr'
+    set -x GIT_EDITOR "$EDITOR --remote-wait"
 end
