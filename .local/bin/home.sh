9a545e4eddc4c100b76a10627eec21c72ea7762f
Devin Alvaro <devin.alvaro@gmail.com>
2019-09-29 20:26:54 +0800

    Enhance home.sh

--- .local/bin/home.sh
+++ .local/bin/home.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --rm -it -v $HOME:/home devinalvaro/home
+cmd='fish'
+if [ $# -ne 0 ]; then
+    cmd="${@:1}"
+fi
+
+docker run --rm -it -v $HOME:/home devinalvaro/home /bin/fish -c "cd $(dirs); $cmd"
