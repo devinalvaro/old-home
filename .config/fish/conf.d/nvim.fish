78bd40c983d6b7486385ef85d369bf66015c3c13
Devin Alvaro <devin.alvaro@gmail.com>
2019-10-11 00:09:15 +0800

    Suppress nvr message

--- .config/fish/conf.d/nvim.fish
+++ .config/fish/conf.d/nvim.fish
@@ -3,6 +3,6 @@ type -q vim ; and set -x EDITOR 'vim'
 type -q nvim; and set -x EDITOR 'nvim'
 
 if set -q NVIM_LISTEN_ADDRESS; and type -q nvr;
-    set -x EDITOR 'nvr'
+    set -x EDITOR 'nvr -s'
     set -x GIT_EDITOR "$EDITOR --remote-wait"
 end
