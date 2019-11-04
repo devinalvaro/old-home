15e88e664f4b67c7dd39554b8c5d5dd0100d2c59
Devin Alvaro <devin.alvaro@gmail.com>
2019-11-04 18:27:09 +0800

    Add service/index parameter to pprof.sh

--- .local/bin/pprof.sh
+++ .local/bin/pprof.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 [profile/heap] <task>"
+if [ "$#" -ne 2 ] && [ "$#" -ne 3 ]; then
+    echo "Usage: $(basename $0) [profile/heap] <task|service index>"
     exit
 fi
 
 COMMAND="$1"
 TASK="$2"
+
+if [ "$#" -eq 3 ]; then
+    SERVICE=$2
+    INDEX=$3
+
+    echo "Getting the task ID of $SERVICE $INDEX ..."
+    TASK=$(smc -e live services show "$SERVICE" | grep "\ $INDEX\ \ \ \ " | grep -o "$SERVICE\.\d\+\.\d\+")
+fi
 
 echo "Profiling $COMMAND on $TASK ..."
 smc -e live tasks run $TASK \
