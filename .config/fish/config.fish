78817fde360af55ecd49467772686cb1dc000ba3
Devin Alvaro <devin.alvaro@gmail.com>
2020-01-16 23:10:59 +0800

    Add lein to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,6 +32,7 @@ set -x PYTHONUSERBASE "$HOME/.pip"
 set -x PATH "$HOME/.cargo/bin:$PATH"
 set -x PATH "$HOME/.fzf/bin:$PATH"
 set -x PATH "$HOME/.go/bin:$PATH"
+set -x PATH "$HOME/.lein/bin:$PATH"
 set -x PATH "$HOME/.local/bin:$PATH"
 set -x PATH "$HOME/.pip/bin:$PATH"
 
