fe1a7d041d9ade6c6d68ef401151b22fbd372847
Devin Alvaro <devin.alvaro@gmail.com>
2020-01-23 22:37:33 +0800

    Fix fzf colon issue

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,6 +1,4 @@
 set -x FZF_DEFAULT_OPTS '--ansi --exact --inline-info'
-set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --delimiter : --nth -1.."
-set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview='cat {}' --preview-window=hidden --bind=alt-p:toggle-preview"
 
 if type -q fd;
     set -x FZF_DEFAULT_COMMAND 'fd --type=file --color=always --follow --hidden'
