7827d6ad808d22a48126fe0b4b71b0906b7054b7
Devin Alvaro <devin.alvaro@gmail.com>
2020-02-12 19:36:59 +0800

    Add less and mysql to cleanup.sh

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -5,6 +5,12 @@ set -eu
 # fzf
 rm -f "$HOME/.local/share/fzf/fzf_history/*"
 
+# less
+rm "$HOME/.lesshst" 2> /dev/null && touch "$_"
+
+# mysql
+rm "$HOME/.mysql_history" 2> /dev/null && touch "$_"
+
 # neovim
 rm -f "$HOME/.local/share/nvim/shada/main.shada"
 rm -f "$HOME/.local/share/nvim/undo/*"
