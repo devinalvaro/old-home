b68a966444d097a3dec02d3b70c703bf19f72bbb
Devin Alvaro <devin.alvaro@gmail.com>
2020-02-18 15:11:22 +0800

    Fix wildcard expansion in scripts

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # fzf
-rm -f "$HOME/.local/share/fzf/fzf_history/*"
+rm -f "$HOME/.local/share/fzf/fzf_history/"*
 
 # less
 rm "$HOME/.lesshst" 2> /dev/null && touch "$_"
@@ -13,5 +13,5 @@ rm "$HOME/.mysql_history" 2> /dev/null && touch "$_"
 
 # neovim
 rm -f "$HOME/.local/share/nvim/shada/main.shada"
-rm -f "$HOME/.local/share/nvim/undo/*"
-rm -f "$HOME/.local/share/nvim/view/*"
+rm -f "$HOME/.local/share/nvim/undo/"*
+rm -f "$HOME/.local/share/nvim/view/"*
