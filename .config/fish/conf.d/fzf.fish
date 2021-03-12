4558121ee5b1798e2cfc37ccd1390494a368d2f2
Devin Alvaro <devin.alvaro@gmail.com>
2021-03-12 11:27:41 +0800

    Remove ignore files

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -2,5 +2,5 @@ set -x FZF_DEFAULT_OPTS '--ansi --inline-info'
 
 if type -q fd;
     set -x FZF_DEFAULT_COMMAND 'fd --type=file --color=always --follow --hidden'
-    set -x FZF_DIRECTORY_COMMAND 'fd --type=directory --color=always --follow --no-ignore-vcs'
+    set -x FZF_DIRECTORY_COMMAND 'fd --type=directory --color=always --follow'
 end
